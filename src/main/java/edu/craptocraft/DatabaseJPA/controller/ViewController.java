fd00a43b7f684f8e4ee72efd83897cbfc558d159
isaacvt01 <_vasqueztucent@cifpfbmoll.eu>
2023-04-21 20:34:13 +0200

    fix edit view

--- src/main/java/edu/craptocraft/DatabaseJPA/controller/ViewController.java
+++ src/main/java/edu/craptocraft/DatabaseJPA/controller/ViewController.java
@@ -8,10 +8,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
+@SessionAttributes("task")
 public class ViewController {
     @Autowired
     private ToDoRepository toDoRepository;
@@ -30,8 +34,9 @@ public class ViewController {
         return "addTask";
     }
     @PostMapping("/addTask")
-    public String addTask(Task task) {
+    public String addTask(Task task, SessionStatus status) {
         toDoRepository.save(task);
+        status.setComplete();
         return "redirect:/allTasks";
     }
     @GetMapping("/deleteTask/{id}")
@@ -39,9 +44,15 @@ public class ViewController {
         toDoRepository.deleteById(id);
         return "redirect:/allTasks";
     }
-    @GetMapping("/editTask/{id}")
+    @GetMapping("/addTask/{id}")
     public String editTask(@PathVariable("id") long id, Model model) {
-        Task task = toDoRepository.findById(id).orElse(new Task());
+        Task task = null;
+        if (id > 0) {
+            Optional<Task> optionalTask = toDoRepository.findById(id);
+            if (optionalTask.isPresent()) {
+                task = optionalTask.get();
+            }
+        }
         model.addAttribute("task", task);
         model.addAttribute("title", "Edit task");
         return "addTask";
